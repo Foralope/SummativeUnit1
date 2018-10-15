d828d3294fd9a8573bfdfa15fec93883f4b1511c
Foralope <43345303+Foralope@users.noreply.github.com>
2018-10-15 10:21:09 -0400

    Update NewJFrame.java

--- src/NewJFrame.java
+++ src/NewJFrame.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Will Zwart
+ * 15/10/18
+ * A short 5 question multiple choice quiz with a random number.
  */
 
 /**
